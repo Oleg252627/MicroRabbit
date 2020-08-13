c02a4dae3298e90d4f70c8ea2e66a407c43ad515
Oleg <opletinenko@gmail.com>
2020-08-13 21:11:54 +0300

    add Publish and SengCommand

--- MicroRabbit/MicroRabbit.Banking.Aplication/Services/AccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Aplication/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using MicroRabbit.Banking.Aplication.Interfaces;
+using MicroRabbit.Banking.Aplication.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,13 +13,25 @@ namespace MicroRabbit.Banking.Aplication.Services
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
-        public AccountService(IAccountRepository accountRepository)
+        private readonly IEventBus _bus;
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
             _accountRepository = accountRepository;
+            _bus = bus;
         }
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            CreateTransferCommand createTransferCommand = new CreateTransferCommand(
+                accountTransfer.FromAccount,
+                accountTransfer.ToAccount,
+                accountTransfer.TransferAmount
+                );
+            _bus.SendCommand(createTransferCommand);
         }
     }
 }
